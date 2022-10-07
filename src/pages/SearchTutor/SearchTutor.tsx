546ce3c469dd51a71cd6949cce5a2dcdde83bec8
nadine <nadine.heng@ncs.com.sg>
2022-10-07 17:53:24 +0800

    remove log

--- src/pages/SearchTutor/SearchTutor.tsx
+++ src/pages/SearchTutor/SearchTutor.tsx
@@ -40,7 +40,7 @@ const SearchTutor = () => {
             <Card className="flex flex-column px-4 py-4 w-8">
                 <SearchBar setTutorList={setTutorList} />
                 {tutorList && tutorList?.length > 0 ? tutorList?.map((tutor, idx) => {
-                    console.log(tutor.introduction)
+                    // console.log(tutor.introduction)
                     return <TutorCard key={idx} intro={tutor.introduction} certs={tutor.certificates} tutorId={tutor.id} subject={tutor.subjects} name={tutor.displayName} />
                 }) : <p className="text-center">No tutors found.</p>}
                 <Paginator rows={10} totalRecords={120} first={0} onPageChange={(e) => { }}></Paginator>
