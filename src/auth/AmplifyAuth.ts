2b980780f92e4365896b2fc45fe6cbd418228629
nadine <nadine.heng@ncs.com.sg>
2023-09-29 15:18:03 +0800

    get env variables from parameter store

--- src/auth/AmplifyAuth.ts
+++ src/auth/AmplifyAuth.ts
@@ -1,12 +1,14 @@
 import { Amplify, Hub, Logger } from "aws-amplify";
 
 const isDev = false
+
 const USER_POOL_ID = process.env.REACT_APP_COGNITO_USER_POOL_ID
 const CLIENT_ID = process.env.REACT_APP_COGNITO_CLIENT_ID
 const COGNITO_DOMAIN = process.env.REACT_APP_COGNITO_DOMAIN
 const HOST_DOMAIN = process.env.REACT_APP_HOST_DOMAIN
 const REDIRECT_URI = process.env.REACT_APP_COGNITO_REDIRECT_URI
 
+console.log("Env variables = ", "USER_POOL_ID", USER_POOL_ID, "CLIENT_ID", CLIENT_ID, "COGNITO_DOMAIN", COGNITO_DOMAIN, "HOST_DOMAIN", HOST_DOMAIN, "REDIRECT_URI", REDIRECT_URI)
 
 Amplify.configure({
   Auth: {
